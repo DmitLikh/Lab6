5fe7f566e68dde95ad8a23a3ec74a4e6a6717c80
likhd <likhd@DESKTOP-FNTQAMI>
2024-03-25 23:04:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str2 = "";
+            foreach (char c in str)
+            if(str2 == c + str2)
+            Console.WriteLine("Является палиндромом");
+            else
+            Console.WriteLine("Не является палиндромом");
+            Console.WriteLine(str2);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+            
+        
+    
+
